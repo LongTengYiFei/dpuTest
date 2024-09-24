31cc8614f1661a60ddcf744693045f7c6344cbbf
chenyifei <378802033@qq.com>
2024-09-24 16:11:43 +0000

    dma copy dpu / batch get

--- dma/dma_copy_dpu/dma_copy_dpu_sample.c
+++ dma/dma_copy_dpu/dma_copy_dpu_sample.c
@@ -29,10 +29,16 @@
 
 #include "dma_common.h"
 
+#include <sys/time.h>
+
 DOCA_LOG_REGISTER(DMA_COPY_DPU);
 
 #define SLEEP_IN_NANOS (10 * 1000) /* Sample the task every 10 microseconds  */
 #define RECV_BUF_SIZE (512)	   /* Buffer which contains config information */
+
+#define KB (1024ul)
+#define MB (1024ul*KB)
+#define GB (1024ul*MB)
 
 /*
  * Saves export descriptor and buffer information content into memory buffers
@@ -149,11 +155,8 @@ doca_error_t dma_copy_dpu(char *export_desc_file_path, char *buffer_info_file_pa
 {
 	struct dma_resources resources;
 	struct program_core_objects *state = &resources.state;
-	struct doca_dma_task_memcpy *dma_task = NULL;
-	struct doca_task *task = NULL;
 	union doca_data task_user_data = {0};
-	struct doca_buf *src_doca_buf = NULL;
-	struct doca_buf *dst_doca_buf = NULL;
+
 	struct doca_mmap *remote_mmap = NULL;
 	char export_desc[1024] = {0};
 	char *remote_addr = NULL;
@@ -166,8 +169,18 @@ doca_error_t dma_copy_dpu(char *export_desc_file_path, char *buffer_info_file_pa
 	};
 	doca_error_t result, tmp_result, task_result;
 
+	int batch_size = 32;
+	int payload_size = 256 * KB;
+	int full_queue_size = payload_size * batch_size;
+	
+
+	struct doca_dma_task_memcpy **dma_task = (struct doca_dma_task_memcpy **)malloc(batch_size * sizeof(struct doca_dma_task_memcpy*));
+	struct doca_task **task = (struct doca_task **)malloc(batch_size * sizeof(struct doca_task*));
+	struct doca_buf **src_doca_buf = (struct doca_buf **)malloc(batch_size * sizeof(struct doca_buf*));
+	struct doca_buf **dst_doca_buf = (struct doca_buf **)malloc(batch_size * sizeof(struct doca_buf*));
+
 	/* Allocate resources */
-	result = allocate_dma_resources(pcie_addr, &resources);
+	result = allocate_dma_resources(pcie_addr, &resources, 2*batch_size);
 	if (result != DOCA_SUCCESS) {
 		DOCA_LOG_ERR("Failed to allocate DMA resources: %s", doca_error_get_descr(result));
 		return result;
@@ -186,13 +199,6 @@ doca_error_t dma_copy_dpu(char *export_desc_file_path, char *buffer_info_file_pa
 		goto destroy_resources;
 	}
 
-	/* Get maximum buffer size allowed */
-	result = doca_dma_cap_task_memcpy_get_max_buf_size(doca_dev_as_devinfo(state->dev), &max_buffer_size);
-	if (result != DOCA_SUCCESS) {
-		DOCA_LOG_ERR("Failed to get max buffer size: %s", doca_error_get_descr(result));
-		goto stop_dma;
-	}
-
 	/* Copy all relevant information into local buffers */
 	result = save_config_info_to_buffers(export_desc_file_path,
 					     buffer_info_file_path,
@@ -205,28 +211,14 @@ doca_error_t dma_copy_dpu(char *export_desc_file_path, char *buffer_info_file_pa
 		goto stop_dma;
 	}
 
-	if (remote_addr_len == 0) {
-		result = DOCA_ERROR_EMPTY;
-		DOCA_LOG_ERR("Remote buffer from host is zero: failed to parse the buffer information file");
-		goto stop_dma;
-	} else if (remote_addr_len > max_buffer_size) {
-		result = DOCA_ERROR_TOO_BIG;
-		DOCA_LOG_ERR("Remote buffer from Host exceeds max allowed buffer: %zu > %zu",
-			     remote_addr_len,
-			     max_buffer_size);
-		goto stop_dma;
-	}
-
-	/* Copy the entire host buffer */
-	dst_buffer_size = remote_addr_len;
-	dpu_buffer = (char *)malloc(dst_buffer_size);
+	dpu_buffer = (char *)malloc(full_queue_size);
 	if (dpu_buffer == NULL) {
 		result = DOCA_ERROR_NO_MEMORY;
 		DOCA_LOG_ERR("Failed to allocate buffer memory: %s", doca_error_get_descr(result));
 		goto stop_dma;
 	}
 
-	result = doca_mmap_set_memrange(state->dst_mmap, dpu_buffer, dst_buffer_size);
+	result = doca_mmap_set_memrange(state->dst_mmap, dpu_buffer, full_queue_size);
 	if (result != DOCA_SUCCESS) {
 		DOCA_LOG_ERR("Failed to set memory range for destination mmap: %s", doca_error_get_descr(result));
 		goto free_dpu_buffer;
@@ -246,81 +238,112 @@ doca_error_t dma_copy_dpu(char *export_desc_file_path, char *buffer_info_file_pa
 		goto free_dpu_buffer;
 	}
 
-	/* Construct DOCA buffer for each address range */
-	result = doca_buf_inventory_buf_get_by_addr(state->buf_inv,
-						    remote_mmap,
-						    remote_addr,
-						    remote_addr_len,
-						    &src_doca_buf);
-	if (result != DOCA_SUCCESS) {
-		DOCA_LOG_ERR("Unable to acquire DOCA buffer representing remote buffer: %s",
-			     doca_error_get_descr(result));
-		goto destroy_remote_mmap;
-	}
-
-	/* Construct DOCA buffer for each address range */
-	result = doca_buf_inventory_buf_get_by_addr(state->buf_inv,
-						    state->dst_mmap,
-						    dpu_buffer,
-						    dst_buffer_size,
-						    &dst_doca_buf);
-	if (result != DOCA_SUCCESS) {
-		DOCA_LOG_ERR("Unable to acquire DOCA buffer representing destination buffer: %s",
-			     doca_error_get_descr(result));
-		goto destroy_src_buf;
-	}
-
-	/* Set data position in src_buff */
-	result = doca_buf_set_data(src_doca_buf, remote_addr, dst_buffer_size);
-	if (result != DOCA_SUCCESS) {
-		DOCA_LOG_ERR("Failed to set data for DOCA source buffer: %s", doca_error_get_descr(result));
-
-		goto destroy_dst_buf;
+	for(int i=0; i<=batch_size-1; i++){
+		/* Construct DOCA buffer for each address range */
+		// 这个src地址映射没办法了，就是这么奇怪
+		result = doca_buf_inventory_buf_get_by_addr(state->buf_inv,
+								remote_mmap,
+								remote_addr,
+								payload_size,
+								&src_doca_buf[i]);
+		if (result != DOCA_SUCCESS) {
+			DOCA_LOG_ERR("Unable to acquire DOCA buffer representing remote buffer: %s",
+					doca_error_get_descr(result));
+			goto destroy_remote_mmap;
+		}
+		
+		/* Construct DOCA buffer for each address range */
+		result = doca_buf_inventory_buf_get_by_addr(state->buf_inv,
+								state->dst_mmap,
+								dpu_buffer + payload_size * i,
+								payload_size,
+								&dst_doca_buf[i]);
+		if (result != DOCA_SUCCESS) {
+			DOCA_LOG_ERR("Unable to acquire DOCA buffer representing destination buffer: %s",
+					doca_error_get_descr(result));
+			goto destroy_src_buf;
+		}
 	}
 
 	/* Include result in user data of task to be used in the callbacks */
 	task_user_data.ptr = &task_result;
 
 	/* Allocate and construct DMA task */
-	result = doca_dma_task_memcpy_alloc_init(resources.dma_ctx,
-						 src_doca_buf,
-						 dst_doca_buf,
-						 task_user_data,
-						 &dma_task);
-	if (result != DOCA_SUCCESS) {
-		DOCA_LOG_ERR("Failed to allocate DMA memcpy task: %s", doca_error_get_descr(result));
-		goto destroy_dst_buf;
-	}
-	/* Number of tasks submitted to progress engine */
-	resources.num_remaining_tasks = 1;
+	for(int i=0; i<=batch_size-1; i++){
+		result = doca_dma_task_memcpy_alloc_init(resources.dma_ctx,
+				src_doca_buf[i],
+				dst_doca_buf[i],
+				task_user_data,
+				&dma_task[i]);
+		if (result != DOCA_SUCCESS) {
+			DOCA_LOG_ERR("Failed to allocate DMA memcpy task: %s", doca_error_get_descr(result));
+			goto destroy_dst_buf;
+		}
 
-	task = doca_dma_task_memcpy_as_task(dma_task);
-
-	/* Submit DMA task */
-	result = doca_task_submit(task);
-	if (result != DOCA_SUCCESS) {
-		DOCA_LOG_ERR("Failed to submit DMA task: %s", doca_error_get_descr(result));
-		doca_task_free(task);
-		goto destroy_dst_buf;
+		task[i] = doca_dma_task_memcpy_as_task(dma_task[i]);
+		if (task[i] == NULL) {
+			result = DOCA_ERROR_UNEXPECTED;
+			DOCA_LOG_ERR("Failed to get DOCA DMA hash task as DOCA task: %s", doca_error_get_descr(result));
+			return result;
+		}
 	}
 
-	resources.run_pe_progress = true;
+	struct timeval start_time, end_time;
+	gettimeofday(&start_time, NULL);
+	long long cost_time_us = 0;
 
-	/* Wait for all tasks to be completed and context stopped */
-	while (resources.run_pe_progress) {
-		if (doca_pe_progress(state->pe) == 0)
-			nanosleep(&ts, &ts);
+	int batch_num = remote_addr_len / full_queue_size;
+	for(int i=0; i<=batch_num - 1; i++){
+		resources.num_remaining_tasks = 0;
+		for(int j=0; j<=batch_size-1; j++){
+			// 调整源地址 remote
+			result = doca_buf_set_data(src_doca_buf[j], remote_addr + full_queue_size*i + payload_size*j, payload_size);
+			if (result != DOCA_SUCCESS) {
+				DOCA_LOG_ERR("Unable to set data in the DOCA buffer representing source buffer: %s",
+						doca_error_get_descr(result));
+				goto destroy_dst_buf;
+			}
+
+			// 重置目标长度为0
+			doca_buf_reset_data_len(dst_doca_buf[j]);
+
+			/* Submit DMA task */
+			resources.num_remaining_tasks++;
+			result = doca_task_submit(task[j]);
+			if (result != DOCA_SUCCESS) {
+				DOCA_LOG_ERR("Failed to submit DMA task: %s", doca_error_get_descr(result));
+				doca_task_free(task[j]);
+				goto destroy_dst_buf;
+			}
+		}
+
+		resources.run_pe_progress = true;
+
+		/* Wait for all tasks to be completed and context stopped */
+		while (resources.run_pe_progress) {
+			if (doca_pe_progress(state->pe) == 0)
+				;
+			result = task_result;
+		}
+
+		DOCA_LOG_INFO("batch %d over", i);
 	}
+
+	gettimeofday(&end_time, NULL);
+	cost_time_us = (end_time.tv_sec - start_time.tv_sec) * 1000000 + (end_time.tv_usec - start_time.tv_usec);
+	DOCA_LOG_INFO("DMA memcpy task cost %lld us", cost_time_us);
 
 	/* Check result of task according to the result we update in the callbacks */
-	if (task_result == DOCA_SUCCESS) {
-		DOCA_LOG_INFO("Remote DMA copy was done Successfully");
-		dpu_buffer[dst_buffer_size - 1] = '\0';
-		DOCA_LOG_INFO("Memory content: %s", dpu_buffer);
-	} else
-		DOCA_LOG_ERR("DMA memcpy task failed: %s", doca_error_get_descr(task_result));
+	// if (task_result == DOCA_SUCCESS) {
+	// 	DOCA_LOG_INFO("Remote DMA copy was done Successfully");
+	// 	dpu_buffer[dst_buffer_size - 1] = '\0';
+	// 	DOCA_LOG_INFO("Memory content: %s", dpu_buffer);
+	// } else
+	// 	DOCA_LOG_ERR("DMA memcpy task failed: %s", doca_error_get_descr(task_result));
 
 	result = task_result;
+	DOCA_LOG_INFO("all batch over");
+	return result;
 
 	/* Inform host that DMA operation is done */
 	DOCA_LOG_INFO("Host sample can be closed, DMA copy ended");
