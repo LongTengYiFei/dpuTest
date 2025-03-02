c87d1beb7d5d73f5a6a74f948b8692726d955eab
chenyifei <378802033@qq.com>
2025-03-02 08:41:24 +0000

    修复了文件读取的bug；cpu解码；

--- proxy/main.cc
+++ proxy/main.cc
@@ -6,9 +6,11 @@
 #include <jerasure/reed_sol.h>
 #include <jerasure/cauchy.h>
 
+
 #include <sys/time.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <openssl/sha.h>
 
 #include "dpuProxy.h"
 
@@ -39,7 +41,7 @@ void writeBlocks(string blocks_dir, bool isDataBlock, int blocks_num, char** blo
     }
 }
 
-void cpuEncodeTest(string input_file_path, int k, int m, int w, int block_size){
+void cpuTest(string input_file_path, int k, int m, int w, int block_size){
     struct timeval start_time, end_time;
     gettimeofday(&start_time, 0);
     int *matrix_RSvandermode = reed_sol_vandermonde_coding_matrix(K, M, W);
@@ -51,6 +53,7 @@ void cpuEncodeTest(string input_file_path, int k, int m, int w, int block_size){
     int fd = open(input_file_path.c_str(), O_RDONLY);
     lseek(fd, 0, SEEK_END);
     size_t data_size = lseek(fd, 0, SEEK_CUR);
+    lseek(fd, 0, SEEK_SET);
     if(data_size % (k * block_size)){
         cout<< "文件长度不是K*block size的整数倍" <<endl;
         cout<< "k = " << k <<endl;
@@ -69,12 +72,15 @@ void cpuEncodeTest(string input_file_path, int k, int m, int w, int block_size){
     for (int i = 0; i < m; i++) 
         coding_blocks[i] = (char *)malloc(block_size);
     
-    // CPU encode
+    // CPU encode and decode
+    int erasures[] = {1, k, -1};
     int data_off = 0;
     int cpu_encoding_time = 0;
+    int cpu_decoding_time = 0;
+
     printf("block size = %d\n", block_size);
     for(int i=0; i<=data_size/stripe_size -1;i++){
-        // prepare stripe
+        /*encode*/
         for (int j = 0; j < k; j++) {
             memcpy(data_blocks[j], input_data + data_off, block_size);
             data_off += block_size;
@@ -87,15 +93,50 @@ void cpuEncodeTest(string input_file_path, int k, int m, int w, int block_size){
                         end_time.tv_usec - start_time.tv_usec;
 
         writeBlocks("./dataBlocks", true, k, data_blocks, block_size); 
-        writeBlocks("./codeBlocks", false, m, coding_blocks, block_size);                       
+        writeBlocks("./codeBlocks", false, m, coding_blocks, block_size);           
+
+        /*decode*/
+        // compute data block sha1
+        unsigned char sha1[SHA_DIGEST_LENGTH];
+        printf("original sha1\n");
+        SHA1((unsigned char*)data_blocks[1], block_size, sha1);
+        printf("SHA1 of data block 1: ");
+        for (int a = 0; a < SHA_DIGEST_LENGTH; a++)
+            printf("%02x", sha1[a]);
+        printf("\n");
+
+        SHA1((unsigned char*)coding_blocks[0], block_size, sha1);
+        printf("SHA1 of code block 0: ");
+        for (int a = 0; a < SHA_DIGEST_LENGTH; a++)
+            printf("%02x", sha1[a]);
+        printf("\n");
+        
+        memset(data_blocks[1], 0, block_size);  // 模拟丢失  数据块 1
+        memset(coding_blocks[0], 0, block_size);    // 模拟丢失 校验块 0
+        gettimeofday(&start_time, 0);
+        jerasure_matrix_decode(k, m, w, matrix_RSvandermode, 0, erasures, data_blocks, coding_blocks, block_size);
+        gettimeofday(&end_time, 0);
+        cpu_decoding_time += (end_time.tv_sec - start_time.tv_sec) * 1000000 + 
+                        end_time.tv_usec - start_time.tv_usec;
+
+        // 恢复后的SHA1值
+        printf("restore sha1\n");
+        SHA1((unsigned char*)data_blocks[1], block_size, sha1);
+        printf("SHA1 of data block 1: ");
+        for (int a = 0; a < SHA_DIGEST_LENGTH; a++)
+            printf("%02x", sha1[a]);
+        printf("\n");
+
+        SHA1((unsigned char*)coding_blocks[0], block_size, sha1);
+        printf("SHA1 of code block 0: ");
+        for (int a = 0; a < SHA_DIGEST_LENGTH; a++)
+            printf("%02x", sha1[a]);
+        printf("\n");
     }
 
     printf("CPU encoding time %d us\n", cpu_encoding_time);
     float cpu_throughput = ((float)data_size / MB) / ((float)cpu_encoding_time / 1000000);
     printf("CPU encoding throughput %.2f MB/s\n", cpu_throughput);
-}
-
-void cpuDecodeTest(){
 
 }
 
@@ -162,7 +203,7 @@ void dpuEncodeTest(string input_file_path, int k, int m, int w, int block_size){
 
 int main(int argc, char** argv) {
     char* file_name = argv[1];
-    //cpuEncodeTest(file_name, K, M, W, BLOCK_SIZE);
-    dpuEncodeTest(file_name, K, M, W, BLOCK_SIZE);
+    cpuTest(file_name, K, M, W, BLOCK_SIZE);
+    //dpuEncodeTest(file_name, K, M, W, BLOCK_SIZE);
     return 0;
 }
